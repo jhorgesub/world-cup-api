d87649ebc0c1083d0a105919782d5c4225703fd1
Jorge Subeldia <subel.jorgeto@gmail.com>
2022-11-07 10:21:06 -0300

    primer commit

---
+++ src/main/java/com/globant/worldcupapi/WorldCupApiApplication.java
@@ -0,0 +1,13 @@
+package com.globant.worldcupapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldCupApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldCupApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/globant/worldcupapi/WorldCupApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globant.worldcupapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldCupApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
